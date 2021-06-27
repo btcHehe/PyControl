07945f928fbf7f791f3e75cd08898d26a3c68bc5
btc <btc121314@gmail.com>
2021-06-27 21:38:05 +0200

    plotter optimization + obsv() and contr() fix

--- pyControl/plotter.py
+++ pyControl/plotter.py
@@ -1,6 +1,6 @@
-from PyOpenGL.GLUT import *
-from PyOpenGL.GL import *
-from PyOpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 import numpy as np
 import sys
 
@@ -10,6 +10,9 @@ _xmin = _xmax = _ymin = _ymax = 0
 _xlabel = ""
 _ylabel = ""
 _title = ""
+_fastMode = False
+_sampleRate = 0
+
 
 def __init():
     glClearColor(1.0, 1.0, 1.0, 1.0)
@@ -17,18 +20,20 @@ def __init():
     global _xmax
     global _ymin
     global _ymax
-    gluOrtho2D(_xmin, _xmax, 2*_ymin, 2*_ymax)
+    gluOrtho2D(_xmin, _xmax, 2 * _ymin, 2 * _ymax)
+
 
 def __plotting():
     global _title
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
-    glutInitWindowSize(400,400)
-    glutInitWindowPosition(700,700)
+    glutInitWindowSize(400, 400)
+    glutInitWindowPosition(400, 400)
     glutCreateWindow(_title)
     glutDisplayFunc(__plotFunc)
     __init()
     glutMainLoop()
+
 
 def __plotFunc():
     global _x
@@ -37,24 +42,51 @@ def __plotFunc():
     global _xmax
     global _ymin
     global _ymax
+    global _fastMode
+    global _sampleRate
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(0.0, 0.0, 0.0)
+    glLineWidth(2.0)
+
+    # making plot axis
     glBegin(GL_LINES)
-    glVertex2f(_xmin, 0.0)
+    glVertex2f(_xmin, 0.0)  # x axis
     glVertex2f(_xmax, 0.0)
-    glVertex2f(0.0, 2*_ymax)
-    glVertex2f(0.0, 2*_ymin)
+    glVertex2f(0.0, 2 * _ymax)  # y axis
+    glVertex2f(0.0, 2 * _ymin)
+
+    glVertex2f(0.0, 2 * _ymax)  # y axis arrow
+    glVertex2f(0.0 - _xmax / 40, 2 * (_ymax - _ymax / 30))
+    glVertex2f(0.0, 2 * _ymax)
+    glVertex2f(0.0 + _xmax / 40, 2 * (_ymax - _ymax / 30))
+
+    glVertex2f(_xmax, 0.0)  # x axis arrow
+    glVertex2f(_xmax - _xmax / 30, 0.0 + _ymax / 30)
+    glVertex2f(_xmax, 0.0)
+    glVertex2f(_xmax - _xmax / 30, 0.0 - _ymax / 30)
+
     glEnd()
     glFlush()
 
-    for j in range(np.size(_x)-1):
-        glBegin(GL_LINES)
-        glVertex2f(_x[j],_y[j])
-        glVertex2f(_x[j+1],_y[j+1])
-        glEnd()
-        glFlush()
+    glColor3f(0.0, 0.0, 1.0)
+    if _fastMode:
+        j = 0
+        while j < np.size(_x):
+            glBegin(GL_POINTS)
+            glVertex2f(_x[j], _y[j])
+            glEnd()
+            glFlush()
+            j += _sampleRate  # skip size
+    else:
+        for j in range(np.size(_x) - 1):
+            glBegin(GL_LINES)
+            glVertex2f(_x[j], _y[j])
+            glVertex2f(_x[j + 1], _y[j + 1])
+            glEnd()
+            glFlush()
 
-def plot(x,y,xlabel="x",ylabel="y",title="plot of y(x)"):
+
+def plot(x, y, xlabel="x", ylabel="y", title="plot of y(x)", fastMode=False, sampleRate=0):
     global _x
     global _y
     global _xmin
@@ -64,6 +96,8 @@ def plot(x,y,xlabel="x",ylabel="y",title="plot of y(x)"):
     global _xlabel
     global _ylabel
     global _title
+    global _fastMode
+    global _sampleRate
     _x = np.copy(x)
     _y = np.copy(y)
     _xmin = np.amin(_x)
@@ -73,9 +107,20 @@ def plot(x,y,xlabel="x",ylabel="y",title="plot of y(x)"):
     _xlabel = xlabel
     _ylabel = ylabel
     _title = title
+    _fastMode = fastMode
+    if sampleRate == 0 and fastMode:
+        raise Exception("you've chosen fastMode so you need to specify sampling rate with sampleRate argument")
+    else:
+        _sampleRate = sampleRate
     __plotting()
 
-def plotMore(xvect,yvect,xlabelvect=[],ylabelvect=[],title="multiple plots"):   #TODO  plot multiple functions at one plane
+
+def plotMore(xvect, yvect, xlabelvect=None, ylabelvect=None,
+             title="multiple plots"):  # TODO  plot multiple functions at one plane
+    if ylabelvect is None:
+        ylabelvect = []
+    if xlabelvect is None:
+        xlabelvect = []
     global _x
     global _y
     global _xmin
@@ -86,20 +131,8 @@ def plotMore(xvect,yvect,xlabelvect=[],ylabelvect=[],title="multiple plots"):   
     global _ylabel
     global _title
     if np.size(xlabelvect) < np.size(xvect):
-        for i in range(np.size(xvect)-np.size(xlabelvect)):
-            np.append(xlabelvect,f'x{i+1}')
-    if  np.size(ylabelvect) < np.size(yvect):
-        for j in range(np.size(yvect)-np.size(ylabelvect)):
-            np.append(ylabelvect,f'y{j+1}')
-    
-
-
-
-x = np.array([])
-y = np.array([])
-
-x = np.arange(-100.0, 100.0, 0.01)
-for i in range(np.size(x)):
-    y = np.append(y, np.sin(x[i]))
-
-plot(x,y)
+        for i in range(np.size(xvect) - np.size(xlabelvect)):
+            np.append(xlabelvect, f'x{i + 1}')
+    if np.size(ylabelvect) < np.size(yvect):
+        for j in range(np.size(yvect) - np.size(ylabelvect)):
+            np.append(ylabelvect, f'y{j + 1}')
--- pyControl/pyControl.py
+++ pyControl/pyControl.py
@@ -4,24 +4,25 @@ import plotter as plt
 
 
 # denumerator and numerator coefficients must be given in a specific order:
-#   a1*s^n + a2*s^(n-1) + a3*s^(n-2) + a4*s^(n-3) + ... =>  [a1, a2, a3, a4,...]
+#   numerator: a1*s^n + a2*s^(n-1) + a3*s^(n-2) + a4*s^(n-3) + ... =>  [a1, a2, a3, a4,...]
+#   denumerator: s^n + a1*s^(n-1) + a2*s^(n-2) + a3*s^(n-3) + ... => [a1, a2, a3, ...]
 
 class sys:
     def __init__(self):
-        self.A = np.array()
-        self.B = np.array()
-        self.C = np.array()
-        self.D = np.array()
+        self.A = np.array([])
+        self.B = np.array([])
+        self.C = np.array([])
+        self.D = np.array([])
         self.TFnumerator = np.array([])
         self.TFdenumerator = np.array([])
         self.startCond = np.array([])
 
     # diagonal form
-    def diag(self):
+    def diag(self):  # TODO fix this
         eigVals, eigVecs = np.linalg.eig(self.A)
-        P = np.array()  # transformation matrix
+        P = np.empty((np.size(eigVecs[:, 0]), 0))  # transformation matrix
         for eigenVector in eigVecs:
-            P = np.append(P, np.array(eigenVector).transpose(), axis=1)
+            P = np.column_stack((P, eigenVector))
         invP = np.linalg.inv(P)
         A = invP * self.A * P
         B = invP * self.B
@@ -32,50 +33,42 @@ class sys:
     # highest power of the s in denumerator must be bigger than highest power in the numerator
     # highest power of s in denumerator must have coefficient equal 1
     def obsv(self):
-        numCols = self.TFnumerator.shape()
-        denCols = self.TFdenumerator.shape()
-        if denCols > numCols:           #making numerator and denumerator equal length filling with 0
-            tempArr = np.array()
-            np.append(tempArr, np.zeros(denCols - numCols))
-            np.append(tempArr, self.TFnumerator)
+        numCols = np.size(self.TFnumerator)
+        denCols = np.size(self.TFdenumerator)
+        if denCols > numCols:  # making numerator and denumerator equal length filling with 0
+            tempArr = np.zeros(denCols)
+            tempArr[denCols - numCols:] = self.TFnumerator
             self.TFnumerator = tempArr
         elif denCols < numCols:
             raise Exception('denumerator must be higher order than numerator')
-        obsvA = np.array()
-        obsvB = np.array()
-        obsvC = np.array()
-        obsvA = np.append(obsvA, -1 * self.TFdenumerator.transpose(), axis=1)    #column of minus denumerator values
-        subMat = np.identity(denCols - 1)                               #identity matrix
-        np.append(subMat, np.zeros(denCols - 1), axis=0)                #row of 0 added to identity matrix
+        obsvA = -1 * self.TFdenumerator.transpose()  # column of minus denumerator values
+        subMat = np.identity(denCols - 1)  # identity matrix
+        subMat = np.vstack((subMat, np.zeros(denCols - 1)))  # row of 0 added to identity matrix
         obsvA = np.append(obsvA, subMat, axis=1)
-        obsvB = np.append(obsvB, self.TFnumerator.transpose(), axis=1)  #column of numerator values
-        obsvC = np.append(obsvC, np.array([1]), axis=1)
-        obsvC = np.append(obsvC, np.zeros(denCols - 1), axis=1)         #row of 0
+        obsvB = self.TFnumerator[..., None]  # column of numerator values
+        obsvC = np.array([1])
+        obsvC = np.append(obsvC, np.zeros(denCols - 1), axis=0)  # row of 0
         return obsvA, obsvB, obsvC
 
     # controllable canonical form of SISO system
     # highest power of the s in denumerator must be bigger than highest power in the numerator
     # highest power of s in denumerator must have coefficient equal 1
     def contr(self):
-        numCols = np.shape(self.TFnumerator)
-        denCols = np.shape(self.TFdenumerator)
-        if denCols > numCols:                           #making numerator and denumerator equal length
-            tempArr = np.array()
-            np.append(tempArr, np.zeros(denCols - numCols))
-            np.append(tempArr, self.TFnumerator)
+        numCols = np.size(self.TFnumerator)
+        denCols = np.size(self.TFdenumerator)
+        if denCols > numCols:  # making numerator and denumerator equal length filling with 0
+            tempArr = np.zeros(denCols)
+            tempArr[denCols - numCols:] = self.TFnumerator
             self.TFnumerator = tempArr
         elif denCols < numCols:
             raise Exception('denumerator must be higher order than numerator')
-        contA = np.array()
-        contB = np.array()
-        contC = np.array()
-        contA = np.append(contA, np.zeros(denCols - 1).transpose(), axis=1)     #column of 0
-        subMat = np.identity(denCols - 1)                                       #identity matrix
-        np.append(contA, subMat, axis=1)
-        np.append(contA, -1 * self.TFdenumerator, axis=0)                       #row of minus denumerator values
-        contB = np.zeros(denCols - 1).transpose()                               #column of 0
-        np.append(contB, np.array([1]), axis=0)
-        contC = np.array(self.TFnumerator)                                      #row of numerator values
+        contA = np.zeros(denCols - 1)[..., None]  # column of 0
+        subMat = np.identity(denCols - 1)  # identity matrix
+        contA = np.append(contA, subMat, axis=1)
+        contA = np.append(contA, -1 * self.TFdenumerator, axis=0)  # row of minus denumerator values
+        contB = np.zeros(denCols - 1)[..., None]  # column of 0
+        contB = np.vstack((contB, np.array([1])))
+        contC = np.array(self.TFnumerator)  # row of numerator values
         return contA, contB, contC
 
 
@@ -88,7 +81,7 @@ class ss(sys):
         self.B = np.array(B)
         self.C = np.array(C)
         self.D = np.array(D)
-        if np.size(np.array(stCond)) < np.size(A, axis=0):          #if starting conditions vector is too short
+        if np.size(np.array(stCond)) < np.size(A, axis=0):  # if starting conditions vector is too short
             self.startCond = np.array(stCond)
             for i in range(np.size(A, axis=0) - np.size(self.startCond)):
                 np.append(self.startCond, np.array([0]))
@@ -99,8 +92,8 @@ class ss(sys):
 class tf(sys):
     def __init__(self, num, denum):
         super().__init__()
-        self.TFnumerator = np.array(num)
-        self.TFdenumerator = np.array(denum)
+        self.TFnumerator = np.array([num])
+        self.TFdenumerator = np.array([denum])
 
 
 def tfToss(system):
