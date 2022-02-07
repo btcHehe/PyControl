c1fdbc566502d5970047c1623daa76b36896825d
btc <btc121314@gmail.com>
2022-02-07 21:03:28 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='PyControl',
+    version='0.1',
+    description='Package for control systems simulation',
+    author='Bartosz Czaja',
+    packages=['PyControl'],
+    install_requires=['wheel', 'numpy', 'matplotlib']
+)
